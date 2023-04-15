f9c127fa8b07aec9f05871968115a5b89ebe0060
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-15 11:21:39 -0700

    Updated and corrected code & test files

--- Python/tests/unittest/test_customer.py
+++ Python/tests/unittest/test_customer.py
@@ -1,27 +1,35 @@
+#!/usr/bin/python3
+'''This module tests the Customer class with unittest'''
+
+
 import unittest
 from customer import Customer
 
 class TestCustomer(unittest.TestCase):
+    '''This class implements unittest testing for Customer class'''
 
     def setUp(self):
+        '''This method defines a customer() instance'''
         self.customer_1 = Customer('John', 'Brad', 5000)
         self.customer_2 = Customer('Tina', 'Smith', 3000)
 
     def test_customer_mail(self):
+        '''This method creates a test for the customer.customer_mail method'''
         self.assertEqual(self.customer_1.customer_mail, 'John.Brad@email.com')
         self.assertEqual(self.customer_2.customer_mail, 'Tina.Smith@email.com')
 
     def test_customer_fullname(self):
+        '''This method creates a test for customer.customer_fullname method'''
         self.assertEqual(self.customer_1.customer_fullname, 'John Brad')
         self.assertEqual(self.customer_2.customer_fullname, 'Tina Smith')
 
     def test_apply_discount(self):
+        '''This method creates a test for customer.apply_discount method'''
         self.customer_1.apply_discount()
         self.customer_2.apply_discount()
 
         self.assertEqual(self.customer_1.purchase, 4750)
         self.assertEqual(self.customer_2.purchase, 2850)
-
 
 if __name__ == '__main__':
     unittest.main()
